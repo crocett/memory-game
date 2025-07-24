2e4febf91496144c429e51bc2a8fa6b2951ee633
crocett <av200101@gmail.com>
2025-07-24 11:12:12 +0300

    основной код

---
+++ src/App.vue
@@ -0,0 +1,128 @@
+<script setup>
+import TurnOverCard from './components/TurnOverCard.vue';
+
+import jesterImg from '../src/assets/jester.png';
+import bardImg from '../src/assets/bard.png';
+import herbalistImg from '../src/assets/herbalist.png';
+import kingImg from '../src/assets/king.png';
+import moonImg from '../src/assets/moon.png';
+import nunImg from '../src/assets/nun.png';
+import starImg from '../src/assets/star.png';
+import visionaryImg from '../src/assets/visionary.png';
+
+import { ref, onUnmounted } from 'vue';
+
+const images =[
+  jesterImg,
+  bardImg,
+  herbalistImg,
+  kingImg,
+  moonImg,
+  nunImg,
+  starImg,
+  visionaryImg
+]
+
+const cardImages = ref(
+  [...images, ...images]
+  .map((image, index) =>({
+    id: index,
+    image: image,
+    isRotate: false,
+    isMatched: false
+  }))
+  .sort(() => Math.random() - 0.5)
+);
+
+let firstCard = null;
+
+const onCardFlip = (card) => {
+  //если карту перевернули или есть совпадение
+  if (card.isRotate || card.isMatched) return;
+  //меняем флаг состояния переворота
+  card.isRotate = true;
+  //первый клик, запоминаем карту
+  if (!firstCard) {
+    firstCard = card;
+    startTimer();
+  }
+  else{
+    const prevCard = firstCard;
+    firstCard = null;
+    //при условии одинаковых картинок меняем флаги нахождения пары
+    if(prevCard.image === card.image){
+      card.isMatched = true;
+      prevCard.isMatched = true;
+          if (checkWin()) {
+            setTimeout
+            (() =>{
+              clearInterval(timeWaste.value);
+              timeWaste.value = null;
+              firstClick.value = false;
+              alert(` Победа! Время: ${timer.value} секунд!`);
+             },600) }
+
+      return
+    }
+
+    else{
+       setTimeout(() => {
+        prevCard.isRotate = false;
+        card.isRotate = false;
+      }, 1000);
+    }
+    firstCard = null;
+  }
+}
+
+const timer = ref(0);
+const timeWaste = ref(null);
+const firstClick = ref(false);
+
+const startTimer = () => {
+  if (firstClick.value || timeWaste.value) return;
+  firstClick.value = true;
+
+  timeWaste.value = setInterval(() => {
+    timer.value++;
+  }, 1000);
+};
+
+onUnmounted(() => {
+  if (timeWaste.value) {
+    clearInterval(timeWaste.value);
+  }
+});
+
+const checkWin = () => {
+  return cardImages.value.every(card => card.isMatched);
+};
+
+
+
+
+</script>
+
+<template>
+  <div class="timer">
+
+  </div>
+  <div class="gameboard">
+      <div class="grid">
+         <TurnOverCard v-for = "card in cardImages"
+         :key="card.id"
+         :image = "card.image"
+         :is-rotate = "card.isRotate"
+         :is-matched = "card.isMatched"
+
+         @flip="() => onCardFlip(card)"
+         >
+        </TurnOverCard>
+
+      </div>
+  </div>
+</template>
+
+<style scoped>
+@import './components/styles.css'
+</style>
---
+++ src/components/TurnOverCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+defineProps({
+   image: {
+    type: String,
+    required: true
+   },
+
+   isRotate: {
+    type: Boolean,
+    required: true
+   },
+
+   isMatched: {
+    type: Boolean,
+    required: true
+   }
+})
+
+
+</script>
+<template>
+    <div class="card" @click="$emit('flip')" :class="{'rotate': isRotate, 'matched': isMatched}">
+
+        <div class="card-rotation">
+
+        <div class="card-front">
+             <img :src="image" alt="front-image" class="card-img">
+            </div>
+        <div class="card-back"></div>
+
+        </div>
+    
+    </div>
+</template>
+
+<style scoped>
+@import './styles.css'
+</style>
